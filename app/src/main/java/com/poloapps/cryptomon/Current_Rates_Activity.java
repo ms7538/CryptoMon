b714aa769f865cf46970d916204b80a5e23a9088
Marko <ms7538@gmail.com>
2017-12-26 16:48:36 -0700

    Rates Activity added.

---
+++ app/src/main/java/com/poloapps/cryptomon/Current_Rates_Activity.java
@@ -0,0 +1,13 @@
+package com.poloapps.cryptomon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Current_Rates_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current__rates_);
+    }
+}
--- app/src/main/java/com/poloapps/cryptomon/HomeActivity.java
+++ app/src/main/java/com/poloapps/cryptomon/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.poloapps.cryptomon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        final Button Agree_btn = findViewById(R.id.rates_btn);
+
+
+        Agree_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                Intent myIntent = new Intent(HomeActivity.this,
+                        Current_Rates_Activity.class);
+                HomeActivity.this.startActivity(myIntent);
+
+            }
+        });
+
     }
 }
