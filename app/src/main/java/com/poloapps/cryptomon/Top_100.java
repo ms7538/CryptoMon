04d5c6e2b2db873b281ccf7f3581d47b9da173c6
Marko <ms7538@gmail.com>
2018-02-11 18:18:45 -0700

    Dollar / Euro menu and logic complete!

--- app/src/main/java/com/poloapps/cryptomon/Top_100.java
+++ app/src/main/java/com/poloapps/cryptomon/Top_100.java
@@ -1,16 +1,23 @@
 package com.poloapps.cryptomon;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.Uri;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -33,7 +40,7 @@ import java.util.Objects;
 
 public class Top_100 extends AppCompatActivity {
 
-    String LC_url = "https://api.coinmarketcap.com/v1/ticker/";
+    String LC_url = "https://api.coinmarketcap.com/v1/ticker/?convert=EUR";
     ProgressDialog dialog;
     ArrayList<HashMap<String, String>> rankList;
 
@@ -46,6 +53,9 @@ public class Top_100 extends AppCompatActivity {
     @Override
     public void onResume() {
         super.onResume();
+
+        final SharedPreferences mSettings = this.getSharedPreferences("Settings", 0);
+        final Boolean Dollar = mSettings.getBoolean("Dollar", true);
         final Integer RED = ContextCompat.getColor(getApplicationContext(),(R.color.red));
 
         final DecimalFormat formatter  = new DecimalFormat("#,###,###,###.##");
@@ -61,7 +71,15 @@ public class Top_100 extends AppCompatActivity {
                     public void onResponse(String string) {
 
                         try {
+                            String price_key      = "price_usd";
+                            String curr_symbol    = "$";
+                            String volume_24h_key = "24h_volume_usd";
 
+                            if(!Dollar){
+                                price_key      = "price_eur";
+                                curr_symbol    = "\u20AC";
+                                volume_24h_key = "24h_volume_eur";
+                            }
                             rankList = new ArrayList<>();
                             ListView lv = findViewById(R.id.list);
 
@@ -71,13 +89,15 @@ public class Top_100 extends AppCompatActivity {
 
                                 JSONObject obj1 = T100_Array.getJSONObject(i);
 
-                                String rate       = obj1.getString("price_usd");
+                                String rate       = obj1.getString(price_key);
                                 Double d_rate     = Double.parseDouble(rate);
                                 //
-                                String volume_24h = "$" + formatter.format(Double.parseDouble(
-                                                          obj1.getString("24h_volume_usd")));
-                                if (d_rate < .01) rate  = "$" + formatter2.format(d_rate);
-                                else              rate  = "$" + formatter.format(d_rate);
+                                String volume_24h = curr_symbol +
+                                                    formatter.format(Double.parseDouble
+                                                            (obj1.getString(volume_24h_key)));
+
+                                if (d_rate < .01) rate  = curr_symbol + formatter2.format(d_rate);
+                                else              rate  = curr_symbol + formatter.format(d_rate);
 
                                 String name       = obj1.getString("name");
                                 String symbol     = obj1.getString("symbol");
@@ -109,38 +129,39 @@ public class Top_100 extends AppCompatActivity {
                             ListAdapter listAdapter = new SimpleAdapter(getApplicationContext(),
                                     rankList, R.layout.list_item, from, to)
                             {
+
                                 @Override
-                                public View getView(int position, View cnvrtView, ViewGroup parent){
+                                public View getView(int position, View cnvrtView, ViewGroup parent) {
 
                                     View view = super.getView(position, cnvrtView, parent);
 
                                     TextView delta_1h = view.findViewById(R.id.h1);
                                     TextView delta_1d = view.findViewById(R.id.d1);
                                     TextView delta_7d = view.findViewById(R.id.delta7_d);
-                                    final TextView link_id  = view.findViewById(R.id.id_link);
+                                    final TextView link_id = view.findViewById(R.id.id_link);
 
                                     Map<String, String> currentRow = rankList.get(position);
 
-                                    double    delta1h = 0;
-                                    if (!Objects.equals(currentRow.get("d1h"), "null")){
+                                    double delta1h = 0;
+                                    if (!Objects.equals(currentRow.get("d1h"), "null")) {
                                         delta1h = Double.parseDouble(currentRow.get("d1h"));
                                     }
-                                    if      ( delta1h < 0 ) delta_1h.setTextColor(RED);
-                                    else if ( delta1h > 0 ) delta_1h.setTextColor(Color.GREEN);
+                                    if (delta1h < 0) delta_1h.setTextColor(RED);
+                                    else if (delta1h > 0) delta_1h.setTextColor(Color.GREEN);
 
-                                    double    delta1d = 0;
-                                    if (!Objects.equals(currentRow.get("d1d"), "null")){
+                                    double delta1d = 0;
+                                    if (!Objects.equals(currentRow.get("d1d"), "null")) {
                                         delta1d = Double.parseDouble(currentRow.get("d1d"));
                                     }
-                                    if      ( delta1d < 0 ) delta_1d.setTextColor(RED);
-                                    else if ( delta1d > 0 ) delta_1d.setTextColor(Color.GREEN);
+                                    if (delta1d < 0) delta_1d.setTextColor(RED);
+                                    else if (delta1d > 0) delta_1d.setTextColor(Color.GREEN);
 
                                     double delta7d = 0;
-                                    if (!Objects.equals(currentRow.get("d7_d"), "null")){
+                                    if (!Objects.equals(currentRow.get("d7_d"), "null")) {
                                         delta7d = Double.parseDouble(currentRow.get("d7_d"));
                                     }
-                                    if      ( delta7d < 0 ) delta_7d.setTextColor(RED);
-                                    else if ( delta7d > 0 ) delta_7d.setTextColor(Color.GREEN);
+                                    if (delta7d < 0) delta_7d.setTextColor(RED);
+                                    else if (delta7d > 0) delta_7d.setTextColor(Color.GREEN);
 
                                     link_id.setOnClickListener(new View.OnClickListener() {
                                         @Override
@@ -167,6 +188,7 @@ public class Top_100 extends AppCompatActivity {
                         dialog.dismiss();
                     }
                 }, new Response.ErrorListener() {
+
             @Override
             public void onErrorResponse(VolleyError volleyError) {
                 Toast.makeText(getApplicationContext(), "Some error occurred!!",
@@ -180,5 +202,65 @@ public class Top_100 extends AppCompatActivity {
         rQueue.add(crypto100_request);
 
    }
+    @Override
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top100_settings_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        final SharedPreferences mSettings = this.getSharedPreferences("Settings", 0);
+        final SharedPreferences.Editor editor = mSettings.edit();
+        final Boolean Dollar = mSettings.getBoolean("Dollar", true);
+
+        switch (item.getItemId()) {
+
+            case R.id.action_currency_sel:
+                AlertDialog.Builder builder;
+
+                builder = new AlertDialog.Builder(Top_100.this);
+                @SuppressLint("InflateParams")
+                View mView2 = getLayoutInflater().inflate(R.layout.currency_diag, null);
+
+                builder.setView(mView2);
+                final AlertDialog dialog2 = builder.create();
+
+                final RadioButton Currency_Dollar = mView2.findViewById(R.id.radio_currency_dollar);
+                final RadioButton Currency_Euro = mView2.findViewById(R.id.radio_currency_euro);
+
+                if (Dollar) Currency_Dollar.setChecked(true);
+                else Currency_Euro.setChecked(true);
+
+                Button Unit_OK = mView2.findViewById(R.id.Units_OK_btn);
+                Unit_OK.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View arg0) {
+
+                        Boolean Dollar_Sel = false;
+                        if (Currency_Dollar.isChecked()) Dollar_Sel = true;
+                        editor.putBoolean("Dollar", Dollar_Sel);
+                        editor.apply();
+                        dialog2.dismiss();
+                        restart();
+                    }});
+                dialog2.show();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+
+        }
+    }
+
+    void restart(){
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
+    }
 }
