dd51afa75616abac63ca63ef674824b55af4f2ed
Marko <ms7538@gmail.com>
2018-05-18 19:02:38 -0600

    v1.1 Alerts Selection UI in progress!

---
+++ app/src/main/java/com/poloapps/cryptomon/dbCurrentValsHandler.java
@@ -0,0 +1,11 @@
+package com.poloapps.cryptomon;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.Cursor;
+import android.content.Context;
+import android.content.ContentValues;
+
+public class dbCurrentValsHandler {
+
+}
