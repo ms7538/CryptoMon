5444a710c903afa8decc26246977b77a343028df
Marko <ms7538@gmail.com>
2018-05-22 20:56:41 -0600

    v1.1 All Alerts Activity in progress!

--- app/src/main/java/com/poloapps/cryptomon/All_AlertsActivity.java
+++ app/src/main/java/com/poloapps/cryptomon/All_AlertsActivity.java
@@ -11,6 +11,7 @@ public class All_AlertsActivity extends BaseActivity {
     dbPriceHandler  dbPHandler;
     dbVolumeHandler dbVHandler;
     dbCurrentValsHandler dbCVHandler;
+    StringBuilder PAlertArray = new StringBuilder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +22,7 @@ public class All_AlertsActivity extends BaseActivity {
         bar.setDisplayShowTitleEnabled(false);
         bar.setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this,
                 R.color.dark_gray)));
-
+        PAlertArray.setLength(0);
         dbPHandler = new dbPriceHandler(this, null);
         dbVHandler = new dbVolumeHandler(this, null);
         dbCVHandler = new dbCurrentValsHandler(this, null);
@@ -30,30 +31,41 @@ public class All_AlertsActivity extends BaseActivity {
     @Override
     public void onResume() {
         super.onResume();
-
+        PAlertArray.setLength(0);
         final TextView tv1 = findViewById(R.id.tv1);
         final TextView tv2 = findViewById(R.id.tv2);
         final TextView tv3 = findViewById(R.id.tv3);
         String priceAlerts = dbPHandler.dbToString();
-        tv1.setText(priceAlerts);
-        tv2.setText(dbVHandler.databaseToString());
+        //tv1.setText(priceAlerts);
+        //tv2.setText(dbVHandler.databaseToString());
 
         // Integer numPriceAlerts = countLines(priceAlerts);
        // String cv = dbCVHandler.currentPrice("bitcoin");
         String[] splitPAlerts = priceAlerts.split("[\n]");
 
         int len1 = splitPAlerts.length;
-        StringBuilder PAlertArray = new StringBuilder();
+
         int i = 0;
-        while (i<len1){
+        for (i = 0;i<len1;i++){
      //for reach line: query dbP for check, Thresh Val; query dbCV for current price.
             PAlertArray.append(splitPAlerts[i]);
             PAlertArray.append("\n");
+            tv3.append(splitPAlerts[i]);
+            tv3.append("\n");
         }
 
+        if(splitPAlerts[0].equals("")) len1 = 0;
         Toast.makeText(getApplicationContext(), Integer.toString(len1),
                 Toast.LENGTH_SHORT).show();
 
-        //tv3.setText(cv);
+
+    }
+
+    @Override
+    public void onPause(){
+        final TextView tv3 = findViewById(R.id.tv3);
+        super.onPause();
+        PAlertArray.setLength(0);
+        tv3.setText("");
     }
 }
