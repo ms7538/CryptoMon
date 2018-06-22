a900e5ef317316597981c3a6f2ae5cc4a36b0873
Marko <ms7538@gmail.com>
2018-06-21 22:28:09 -0600

    v1.1 WIP service 50%

--- app/src/main/java/com/poloapps/cryptomon/serviceCM.java
+++ app/src/main/java/com/poloapps/cryptomon/serviceCM.java
@@ -2,38 +2,45 @@ package com.poloapps.cryptomon;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class serviceCM extends Service {
-
+    private boolean hasStarted = false;
     private static final String TAG = "com.poloapps.cryptomon";
-
+    final Handler handler = new Handler();
+    Timer timer = new Timer();
+    TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+            handler.post(new Runnable() {
+                public void run() {
+                    try {
+                        hasStarted = true;
+                        Log.i(TAG, "Service is running");
+                        Toast.makeText(getApplicationContext(), "Service is running",
+                                Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                        // error, do something
+                    }
+                }
+            });
+        }
+    };
     public serviceCM() {
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i(TAG,"on Start called");
-        Runnable r = new Runnable() {
-            @Override
-            public void run() {
-                //TODO Implement Timed task and stop at onDestroy
-                //                for(int i = 0; i < 500;i++){
-//                    long futureTime = System.currentTimeMillis() + 2000;
-//                    while (System.currentTimeMillis()< futureTime){
-//                        synchronized (this){
-//                            try{
-//                                wait(futureTime-System.currentTimeMillis());
-//                                Log.i(TAG, "Service is doing something");
-//                            }catch (Exception e){}
-//                        }
-//                    }
-//                }
-            }
-        };
-        Thread cmThread = new Thread(r);
-        cmThread.start();
+        Toast.makeText(getApplicationContext(), "Service started",
+                Toast.LENGTH_SHORT).show();
+        if (hasStarted) timer.schedule(task, 0 , 3000);  // interval of 10 sec
+
         return  Service.START_STICKY;
 
     }
@@ -41,6 +48,10 @@ public class serviceCM extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        task.cancel();
+        hasStarted = false;
+        Toast.makeText(getApplicationContext(), "Service has stopped",
+                Toast.LENGTH_SHORT).show();
     }
 
     @Override
